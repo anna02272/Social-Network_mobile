c75889c51a1f3c51e3b489e0ed2d42825283f0f4
LAPTOP-SC9RDVNE\domon <domonjianna@gmail.com>
2024-09-05 15:38:36 +0200

    Load friend requests by user

--- app/src/main/java/com/example/socialnetwork/adpters/FriendRequestAdapter.kt
+++ app/src/main/java/com/example/socialnetwork/adpters/FriendRequestAdapter.kt
@@ -1,6 +1,7 @@
 package com.example.socialnetwork.adpters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,33 +11,72 @@ import android.widget.TextView
 import com.example.socialnetwork.R
 import com.example.socialnetwork.model.entity.FriendRequest
 
-class FriendRequestAdapter(context: Context, friendRequests: ArrayList<FriendRequest>) :
-    ArrayAdapter<FriendRequest>(context, R.layout.fragment_request, friendRequests) {
+class FriendRequestAdapter(
+    context: Context,
+    friendRequest: ArrayList<FriendRequest>,
+    private val acceptButtonText: String? = null,
+    private val deleteButtonText: String? = null) :
+    ArrayAdapter<FriendRequest>(context, R.layout.fragment_report, friendRequest) {
 
+    interface AcceptButtonClickListener {
+        fun onAcceptButtonClick()
+    }
     interface DeleteButtonClickListener {
         fun onDeleteButtonClick()
     }
 
-    var deleteButtonClickListener: FriendRequestAdapter.DeleteButtonClickListener? = null
+    var acceptButtonClickListener: AcceptButtonClickListener? = null
+    var deleteButtonClickListener: DeleteButtonClickListener? = null
+
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var view = convertView
         val friendRequest: FriendRequest? = getItem(position)
 
         if (view == null) {
-            view = LayoutInflater.from(context).inflate(R.layout.fragment_request, parent, false)
+            view = LayoutInflater.from(context).inflate(R.layout.fragment_report, parent, false)
         }
 
         val userTextView = view!!.findViewById<TextView>(R.id.userTextView)
         val dateTextView = view.findViewById<TextView>(R.id.dateTextView)
+        val reportContentTextView = view.findViewById<TextView>(R.id.reportContentTextView)
+        val reasonContentTextView = view.findViewById<TextView>(R.id.reasonContentTextView)
+        val reportTextView = view.findViewById<TextView>(R.id.reportTextView)
+        val reasonTextView = view.findViewById<TextView>(R.id.reasonTextView)
+        val acceptButton = view.findViewById<Button>(R.id.acceptButton)
         val deleteButton = view.findViewById<Button>(R.id.deleteButton)
 
         friendRequest?.let {
-//            userTextView.text = it.fromUser
-//            dateTextView.text = it.createdAt
+            try {
+                userTextView.text = it.fromUser.firstName + " " + it.fromUser.lastName
+                dateTextView.text = it.created_at.toString()
+
+                reportContentTextView.visibility = View.GONE
+                reasonContentTextView.visibility = View.GONE
+                reportTextView.visibility = View.GONE
+                reasonTextView.visibility = View.GONE
+
+                acceptButton.visibility =
+                    if (acceptButtonText.isNullOrEmpty()) View.GONE else View.VISIBLE
+                acceptButton.text = acceptButtonText
+
+                deleteButton.visibility =
+                    if (deleteButtonText.isNullOrEmpty()) View.GONE else View.VISIBLE
+                deleteButton.text = deleteButtonText
+
+            } catch (e: Exception) {
+                Log.e("FriendRequestAdapter", "Error binding view: ${e.message}", e)
+            }
+        }
+        acceptButton.setOnClickListener {
+            friendRequest?.let {
+                acceptButtonClickListener?.onAcceptButtonClick()
+            }
         }
 
         deleteButton.setOnClickListener {
-            deleteButtonClickListener?.onDeleteButtonClick()
+            friendRequest?.let {
+                deleteButtonClickListener?.onDeleteButtonClick()
+            }
         }
 
         return view
--- app/src/main/java/com/example/socialnetwork/fragments/FriendRequestsFragment.kt
+++ app/src/main/java/com/example/socialnetwork/fragments/FriendRequestsFragment.kt
@@ -1,5 +1,6 @@
 package com.example.socialnetwork.fragments
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -8,12 +9,22 @@ import android.widget.ListView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.socialnetwork.R
+import com.example.socialnetwork.activities.LoginActivity
 import com.example.socialnetwork.adpters.FriendRequestAdapter
+import com.example.socialnetwork.clients.ClientUtils
 import com.example.socialnetwork.model.entity.FriendRequest
+import com.example.socialnetwork.model.entity.User
+import com.example.socialnetwork.utils.PreferencesManager
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class FriendRequestsFragment : Fragment(),
     FriendRequestAdapter.DeleteButtonClickListener,
-    ConfirmationDialogFragment.ConfirmationDialogListener {
+    ConfirmationDialogFragment.ConfirmationDialogListener,
+    FriendRequestAdapter.AcceptButtonClickListener {
+
+    private var userId: Long? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,23 +32,91 @@ class FriendRequestsFragment : Fragment(),
     ): View? {
         val view = inflater.inflate(R.layout.fragment_friend_requests, container, false)
 
-        fillFriendRequestArray(view)
+        val token = PreferencesManager.getToken(requireContext())
+        if (token == null) {
+            val intent = Intent(requireContext(), LoginActivity::class.java)
+            startActivity(intent)
+            requireActivity().finish()
+            return view
+        }
+
+        fetchUserData(token)
 
         return view
     }
-    private fun fillFriendRequestArray(view: View) {
-        val friendRequestsListView = view.findViewById<ListView>(R.id.friendRequestsListView)
+    private fun fetchFriendRequestsFromServer(token: String, userId: Long) {
+        val friendRequestService = ClientUtils.getFriendRequestService(token)
 
-        val friendRequests = ArrayList<FriendRequest>()
+        userId?.let { friendRequestService.getByUser(userId) }
+            ?.enqueue(object : Callback<List<FriendRequest>> {
+                override fun onResponse(
+                    call: Call<List<FriendRequest>>,
+                    response: Response<List<FriendRequest>>
+                ) {
+                    if (response.isSuccessful) {
+                        val friendRequest = response.body() ?: arrayListOf()
+                        updateListView(friendRequest)
+                    } else if (response.code() == 401) {
+                        handleTokenExpired()
+                    } else {
+                        showToast("Failed to load reports")
+                    }
+                }
 
+                override fun onFailure(call: Call<List<FriendRequest>>, t: Throwable) {
+                    showToast("Error: ${t.message}")
+                }
+            })
+    }
 
-        val adapter = FriendRequestAdapter(requireContext(), friendRequests)
+    private fun handleTokenExpired() {
+        PreferencesManager.clearToken(requireContext())
+        val intent = Intent(requireContext(), LoginActivity::class.java)
+        startActivity(intent)
+        requireActivity().finish()
+    }
+    private fun updateListView(friendRequests: List<FriendRequest>) {
+        val listView: ListView = requireView().findViewById(R.id.friendRequestsListView)
+        val adapter = FriendRequestAdapter(
+            requireContext(),
+            ArrayList(friendRequests),
+            getString(R.string.accept),
+            getString(R.string.delete_request)
+        )
+
         adapter.deleteButtonClickListener = this
+        adapter.acceptButtonClickListener = this
+        listView.adapter = adapter
+    }
 
-        friendRequestsListView.adapter = adapter
+    private fun fetchUserData(token: String) {
+        val userService = ClientUtils.getUserService(token)
+        val call = userService.whoAmI()
+
+        call.enqueue(object : Callback<User> {
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                if (response.isSuccessful) {
+                    val user = response.body()
+                    if (user != null) {
+                        userId = user.id
+                        userId?.let { fetchFriendRequestsFromServer(token, it) }
+                    }
+                } else {
+                    showToast("Failed to load user data")
+                }
+            }
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                showToast("Error: ${t.message}")
+            }
+        })
     }
     override fun onDeleteButtonClick() {
         showConfirmationDialog()
+    }
+
+    override fun onAcceptButtonClick() {
+        TODO("Not yet implemented")
     }
     override fun onDialogPositiveClick() {
         Toast.makeText(requireContext(),  "Friend Request deleted", Toast.LENGTH_SHORT).show()
@@ -52,4 +131,8 @@ class FriendRequestsFragment : Fragment(),
         dialog.listener = this
         dialog.show(parentFragmentManager, "ConfirmationDialog")
     }
+    private fun showToast(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
+    }
+
 }
