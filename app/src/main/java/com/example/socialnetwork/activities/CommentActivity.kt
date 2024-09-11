cbb2a36ab64465ff02d38005ff17b9eb381e08ee
LAPTOP-SC9RDVNE\domon <domonjianna@gmail.com>
2024-09-11 12:47:43 +0200

    Create and get comment reactions and load profile image in comments

--- app/src/main/java/com/example/socialnetwork/activities/CommentActivity.kt
+++ app/src/main/java/com/example/socialnetwork/activities/CommentActivity.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageButton
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.content.ContextCompat
@@ -20,6 +21,13 @@ import com.example.socialnetwork.adapters.CommentAdapter
 import com.example.socialnetwork.clients.ClientUtils
 import com.example.socialnetwork.model.entity.Comment
 import com.example.socialnetwork.model.entity.CreateCommentRequest
+import com.example.socialnetwork.model.entity.EReactionType
+import com.example.socialnetwork.model.entity.Reaction
+import com.example.socialnetwork.model.entity.User
+import com.example.socialnetwork.services.CommentService
+import com.example.socialnetwork.services.ReactionService
+import com.example.socialnetwork.services.ReportService
+import com.example.socialnetwork.services.UserService
 import com.example.socialnetwork.utils.PreferencesManager
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.bottomsheet.BottomSheetDialog
@@ -27,13 +35,27 @@ import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.time.LocalDate
 
-class CommentActivity : BottomSheetDialogFragment() {
+class CommentActivity : BottomSheetDialogFragment(),
+    CommentAdapter.ReplyButtonClickListener,
+    CommentAdapter.EditButtonClickListener,
+    CommentAdapter.DeleteButtonClickListener,
+    CommentAdapter.ReportButtonClickListener,
+    CommentAdapter.LikeButtonClickListener,
+    CommentAdapter.DislikeButtonClickListener,
+    CommentAdapter.HeartButtonClickListener
+   {
     private var postId: Long? = null
     private var token: String? = null
     private lateinit var contentEditText: EditText
     private lateinit var errorMessage: TextView
     private var currentToast: Toast? = null
+       private lateinit var commentService: CommentService
+       private lateinit var reactionService: ReactionService
+       private lateinit var userService: UserService
+       private lateinit var reportService: ReportService
+       private var currentUser: User? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -75,8 +97,11 @@ class CommentActivity : BottomSheetDialogFragment() {
         if (token == null || postId == null) {
             handleTokenExpired()
         } else {
+            initializeServices()
             fetchCommentsFromServer(token!!, postId!!)
         }
+
+        fetchUserData()
 
         contentEditText = view.findViewById(R.id.contentEditText)
         errorMessage = view.findViewById(R.id.errorMessage)
@@ -86,16 +111,55 @@ class CommentActivity : BottomSheetDialogFragment() {
         }
 
     }
+       private fun initializeServices() {
+           commentService = ClientUtils.getCommentService(token)
+           reactionService = ClientUtils.getReactionService(token)
+           userService = ClientUtils.getUserService(token)
+           reportService = ClientUtils.getReportService(token)
+       }
+       override fun onReplyButtonClick(comment: Comment) {
+           TODO("Not yet implemented")
+       }
+
+       override fun onEditButtonClick(comment: Comment) {
+           TODO("Not yet implemented")
+       }
+
+       override fun onDeleteButtonClick(comment: Comment) {
+           TODO("Not yet implemented")
+       }
+
+       override fun onReportButtonClick(comment: Comment) {
+           TODO("Not yet implemented")
+       }
+
+       override fun onLikeButtonClick(comment: Comment, view: View) {
+           reactToComment(comment, EReactionType.LIKE, view)
+       }
+
+       override fun onDislikeButtonClick(comment: Comment, view: View) {
+           reactToComment(comment, EReactionType.DISLIKE, view)
+       }
+
+       override fun onHeartButtonClick(comment: Comment, view: View) {
+           reactToComment(comment, EReactionType.HEART, view)
+       }
 
     private fun updateRecyclerView(comments: List<Comment>) {
         val recyclerView: RecyclerView = view?.findViewById(R.id.commentsRecyclerView) ?: return
         val adapter = CommentAdapter(requireContext(), comments)
+        adapter.replyButtonClickListener = this
+        adapter.editButtonClickListener = this
+        adapter.deleteButtonClickListener = this
+        adapter.reportButtonClickListener = this
+        adapter.likeButtonClickListener = this
+        adapter.dislikeButtonClickListener = this
+        adapter.heartButtonClickListener = this
         recyclerView.adapter = adapter
     }
 
 
     private fun fetchCommentsFromServer(token: String, postId: Long) {
-        val commentService = ClientUtils.getCommentService(token)
         val call = commentService.getCommentsByPostId(postId)
 
         call.enqueue(object : Callback<List<Comment>> {
@@ -118,6 +182,25 @@ class CommentActivity : BottomSheetDialogFragment() {
             }
         })
     }
+       private fun fetchUserData() {
+           val call = userService.whoAmI()
+
+           call.enqueue(object : Callback<User> {
+               override fun onResponse(call: Call<User>, response: Response<User>) {
+                   if (response.isSuccessful) {
+                       val user = response.body()
+                       if (user != null) {
+                           currentUser = user
+                       }
+                   } else {
+                   }
+               }
+
+               override fun onFailure(call: Call<User>, t: Throwable) {
+                   showToast("Error: ${t.message}")
+               }
+           })
+       }
 
     private fun handleTokenExpired() {
         PreferencesManager.clearToken(requireContext())
@@ -146,16 +229,13 @@ class CommentActivity : BottomSheetDialogFragment() {
             text = text
         )
 
-        val token = PreferencesManager.getToken(requireContext()) ?: return
-
-        val commentService = ClientUtils.getCommentService(token)
         val call = commentService.create(postId, comment)
 
         call.enqueue(object : Callback<Comment> {
             override fun onResponse(call: Call<Comment>, response: Response<Comment>) {
                 if (response.isSuccessful) {
                     showToast("Comment created successfully")
-                    fetchCommentsFromServer(token, postId)
+                    token?.let { fetchCommentsFromServer(it, postId) }
                     resetUI()
                 } else {
                     handleError(response)
@@ -167,6 +247,31 @@ class CommentActivity : BottomSheetDialogFragment() {
             }
         })
     }
+       private fun reactToComment(comment: Comment, reactionType: EReactionType, view: View) {
+           val likeCountTextView: TextView = view.findViewById(R.id.likeCountTextView)
+           val dislikeCountTextView: TextView = view.findViewById(R.id.dislikeCountTextView)
+           val heartCountTextView: TextView = view.findViewById(R.id.heartCountTextView)
+           val likeButton: ImageButton = view.findViewById(R.id.likeButton)
+           val dislikeButton: ImageButton = view.findViewById(R.id.dislikeButton)
+           val heartButton: ImageButton = view.findViewById(R.id.heartButton)
+           val reaction =
+               currentUser?.let { Reaction(0, reactionType, LocalDate.now(), it, null, comment) }
+           reaction?.let { reactionService.reactToComment(comment.id, it) }
+               ?.enqueue(object : Callback<Reaction> {
+                   override fun onResponse(call: Call<Reaction>, response: Response<Reaction>) {
+                       if (response.isSuccessful) {
+                           CommentAdapter.updateReactionCounts(requireContext(), comment, likeCountTextView, dislikeCountTextView, heartCountTextView)
+                           currentUser?.let {
+                              CommentAdapter.checkUserReaction(requireContext(), it, comment, likeButton, dislikeButton, heartButton) }
+                           showToast("Reacted successfully")
+                       }
+                   }
+
+                   override fun onFailure(call: Call<Reaction>, t: Throwable) {
+                       showToast("Error: ${t.message}")
+                   }
+               })
+       }
     private fun handleError(response: Response<Comment>) {
         val errorBody = response.errorBody()?.string() ?: "Unknown error"
         errorMessage.text = "Creating comment failed: $errorBody"
@@ -182,4 +287,4 @@ class CommentActivity : BottomSheetDialogFragment() {
         contentEditText.text.clear()
         errorMessage.visibility = View.GONE
     }
-}
+   }
--- app/src/main/java/com/example/socialnetwork/adapters/CommentAdapter.kt
+++ app/src/main/java/com/example/socialnetwork/adapters/CommentAdapter.kt
@@ -9,9 +9,23 @@ import android.widget.ImageView
 import android.widget.PopupMenu
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.example.socialnetwork.R
+import com.example.socialnetwork.clients.ClientUtils
 import com.example.socialnetwork.model.entity.Comment
+import com.example.socialnetwork.model.entity.EReactionType
+import com.example.socialnetwork.model.entity.Reaction
+import com.example.socialnetwork.model.entity.User
+import com.example.socialnetwork.utils.CircleTransform
+import com.example.socialnetwork.utils.PreferencesManager
+import com.google.firebase.FirebaseApp
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.squareup.picasso.Picasso
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import java.time.format.DateTimeFormatter
 
 class CommentAdapter(
@@ -19,57 +33,276 @@ class CommentAdapter(
     private val comments: List<Comment>
 ) : RecyclerView.Adapter<CommentAdapter.CommentViewHolder>() {
 
+    private lateinit var storageReference: StorageReference
+    private var currentUser: User? = null
+    private var currentToast: Toast? = null
+    init {
+        initializeFirebaseStorage()
+        fetchUserData(object : UserCallback {
+            override fun onUserFetched(user: User?) {
+                currentUser = user
+                notifyDataSetChanged()
+            }
+        })
+    }
+    private fun initializeFirebaseStorage() {
+        if (FirebaseApp.getApps(context).isEmpty()) {
+            FirebaseApp.initializeApp(context)
+        }
+        storageReference = FirebaseStorage.getInstance().reference
+    }
+
+    interface UserCallback {
+        fun onUserFetched(user: User?)
+    }
+    interface ReplyButtonClickListener {
+        fun onReplyButtonClick(comment: Comment)
+    }
+
+    interface EditButtonClickListener {
+        fun onEditButtonClick(comment: Comment)
+    }
+    interface DeleteButtonClickListener {
+        fun onDeleteButtonClick(comment: Comment)
+    }
+    interface ReportButtonClickListener {
+        fun onReportButtonClick(comment: Comment)
+    }
+
+    interface LikeButtonClickListener {
+        fun onLikeButtonClick(comment: Comment, view: View)
+    }
+
+    interface DislikeButtonClickListener {
+        fun onDislikeButtonClick(comment: Comment, view: View)
+    }
+
+    interface HeartButtonClickListener {
+        fun onHeartButtonClick(comment: Comment, view: View)
+    }
+
+    var replyButtonClickListener: ReplyButtonClickListener? = null
+    var editButtonClickListener: EditButtonClickListener? = null
+    var deleteButtonClickListener: DeleteButtonClickListener? = null
+    var reportButtonClickListener: ReportButtonClickListener? = null
+    var likeButtonClickListener: LikeButtonClickListener? = null
+    var dislikeButtonClickListener: DislikeButtonClickListener? = null
+    var heartButtonClickListener: HeartButtonClickListener? = null
+
     inner class CommentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-        val profileImage: ImageView = itemView.findViewById(R.id.profileImage)
+        val profileImageView: ImageView = itemView.findViewById(R.id.profileImage)
         val usernameTextView: TextView = itemView.findViewById(R.id.usernameTextView)
         val dateTextView: TextView = itemView.findViewById(R.id.dateTextView)
         val contentTextView: TextView = itemView.findViewById(R.id.contentTextView)
         val moreOptionsButton: ImageButton = itemView.findViewById(R.id.moreOptionsButton)
+        val likeButton: ImageButton = itemView.findViewById(R.id.likeButton)
+        val dislikeButton: ImageButton = itemView.findViewById(R.id.dislikeButton)
+        val heartButton: ImageButton = itemView.findViewById(R.id.heartButton)
+        val likeCountTextView: TextView = itemView.findViewById(R.id.likeCountTextView)
+        val dislikeCountTextView: TextView = itemView.findViewById(R.id.dislikeCountTextView)
+        val heartCountTextView: TextView = itemView.findViewById(R.id.heartCountTextView)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommentViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.fragment_comment, parent, false)
         return CommentViewHolder(view)
+
     }
 
     override fun onBindViewHolder(holder: CommentViewHolder, position: Int) {
         val comment = comments[position]
-
+        comment.user?.id?.let { loadProfileImage(it, holder.profileImageView) }
         holder.usernameTextView.text = comment.user?.profileName?.takeIf { it.isNotEmpty() } ?: comment.user?.username
         val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
         holder.dateTextView.text = comment.timeStamp.format(formatter)
         holder.contentTextView.text = comment.text
 
+
         holder.moreOptionsButton.setOnClickListener { view ->
-            showPopupMenu(view)
+            showPopupMenu(view, comment)
         }
+
+        setupButtons(holder, comment)
+        setupReactions(holder, comment)
+    }
+
+
+    private fun setupButtons(holder: CommentViewHolder, comment: Comment) {
+        holder.likeButton.setOnClickListener { likeButtonClickListener?.onLikeButtonClick(comment, holder.itemView) }
+        holder.dislikeButton.setOnClickListener { dislikeButtonClickListener?.onDislikeButtonClick(comment, holder.itemView) }
+        holder.heartButton.setOnClickListener { heartButtonClickListener?.onHeartButtonClick(comment, holder.itemView) }
+    }
+    private fun setupReactions(holder: CommentViewHolder, comment: Comment) {
+        holder.likeCountTextView.text = comment.reactions.count { it.type == EReactionType.LIKE }.toString()
+        holder.dislikeCountTextView.text = comment.reactions.count { it.type == EReactionType.DISLIKE }.toString()
+        holder.heartCountTextView.text = comment.reactions.count { it.type == EReactionType.HEART }.toString()
+
+        updateReactionCounts(
+            context,
+            comment,
+            holder.likeCountTextView,
+            holder.dislikeCountTextView,
+            holder.heartCountTextView
+        )
+        currentUser?.let {
+            checkUserReaction(
+                context,
+                it,
+                comment,
+                holder.likeButton,
+                holder.dislikeButton,
+                holder.heartButton
+            )
+        }
+    }
+    private fun loadProfileImage(userId: Long, profileImageView: ImageView) {
+        val ref = storageReference!!.child("profile_images/$userId")
+        ref.downloadUrl.addOnSuccessListener { uri ->
+            Picasso.get().load(uri).transform(CircleTransform()).into(profileImageView)
+        }.addOnFailureListener {
+            profileImageView.setImageResource(R.drawable.smiley_circle)
+        }
+    }
+
+    companion object {
+        fun updateReactionCounts(context: Context,
+                                 comment: Comment,
+                                 likeCountTextView: TextView,
+                                 dislikeCountTextView: TextView,
+                                 heartCountTextView: TextView) {
+            val token = PreferencesManager.getToken(context) ?: return
+            val reactionService = ClientUtils.getReactionService(token)
+            val call = reactionService.countReactionsByComment(comment.id)
+            call.enqueue(object : Callback<Map<EReactionType, Integer>> {
+                override fun onResponse(
+                    call: Call<Map<EReactionType, Integer>>,
+                    response: Response<Map<EReactionType, Integer>>
+                ) {
+                    if (response.isSuccessful) {
+                        response.body()?.let { reactionCounts ->
+                            likeCountTextView.text =
+                                reactionCounts[EReactionType.LIKE]?.toString() ?: "0"
+                            dislikeCountTextView.text =
+                                reactionCounts[EReactionType.DISLIKE]?.toString() ?: "0"
+                            heartCountTextView.text =
+                                reactionCounts[EReactionType.HEART]?.toString() ?: "0"
+                        }
+                    }
+                }
+
+                override fun onFailure(call: Call<Map<EReactionType, Integer>>, t: Throwable) {
+                    Toast.makeText(context, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
+                }
+            })
+        }
+
+        fun checkUserReaction(context: Context,
+                              currentUser: User,
+                              comment: Comment,
+                              likeButton: ImageButton,
+                              dislikeButton: ImageButton,
+                              heartButton: ImageButton) {
+            currentUser?.id?.let {
+                val token = PreferencesManager.getToken(context) ?: return
+                val reactionService = ClientUtils.getReactionService(token)
+                reactionService.findReactionByCommentAndUser(comment.id, it)?.enqueue(object :
+                    Callback<Reaction?> {
+                    override fun onResponse(call: Call<Reaction?>, response: Response<Reaction?>) {
+                        if (response.isSuccessful) {
+                            val reaction = response.body()
+                            if (reaction != null) {
+                                val reactionTypeColorMap = mapOf(
+                                    EReactionType.LIKE to Pair(likeButton, R.color.blue),
+                                    EReactionType.DISLIKE to Pair(dislikeButton, R.color.blue),
+                                    EReactionType.HEART to Pair(heartButton, R.color.red)
+                                )
+
+                                reactionTypeColorMap.forEach { (type, pair) ->
+                                    val (button, colorResId) = pair
+                                    button.post {
+                                        val color = ContextCompat.getColor(context, colorResId)
+                                        changeReactionColor(context, button, reaction.type == type, color)
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    override fun onFailure(call: Call<Reaction?>, t: Throwable) {
+                        Toast.makeText(context, "Error: ${t.message}", Toast.LENGTH_SHORT).show()
+                    }
+                })
+            }
+        }
+        fun changeReactionColor(context: Context, button: ImageButton, reacted: Boolean, color: Int) {
+            val finalColor = if (reacted) color else ContextCompat.getColor(context, R.color.grey)
+            button.setColorFilter(finalColor)
+        }
+    }
+    private fun fetchUserData(callback: UserCallback) {
+        val token = PreferencesManager.getToken(context) ?: return
+        val userService = ClientUtils.getUserService(token)
+        val call = userService.whoAmI()
+
+        call.enqueue(object : Callback<User> {
+            override fun onResponse(call: Call<User>, response: Response<User>) {
+                if (response.isSuccessful) {
+                    val user = response.body()
+                    if (user != null) {
+                        currentUser = user
+                        callback.onUserFetched(user)
+                    }
+                } else {
+                    callback.onUserFetched(null)
+                }
+            }
+
+            override fun onFailure(call: Call<User>, t: Throwable) {
+                showToast("Error: ${t.message}")
+                callback.onUserFetched(null)
+            }
+        })
     }
 
     override fun getItemCount(): Int = comments.size
 
-    private fun showPopupMenu(view: View) {
+    private fun showPopupMenu(view: View, comment: Comment) {
         val popup = PopupMenu(context, view)
+
+        val menu = popup.menu
+        menu.add(0, R.id.reply, 0, context.getString(R.string.reply_to_comment))
+        menu.add(0, R.id.edit, 1, context.getString(R.string.edit_comment))
+        menu.add(0, R.id.delete, 2, context.getString(R.string.delete_comment))
+        menu.add(0, R.id.report, 3, context.getString(R.string.report_comment))
+
+
         popup.setOnMenuItemClickListener { item ->
             when (item.itemId) {
                 R.id.reply -> {
-                    Toast.makeText(context, "Reply to Comment clicked", Toast.LENGTH_SHORT).show()
+                    replyButtonClickListener?.onReplyButtonClick(comment)
                     true
                 }
                 R.id.edit -> {
-                    Toast.makeText(context, "Edit Comment clicked", Toast.LENGTH_SHORT).show()
+                    editButtonClickListener?.onEditButtonClick(comment)
                     true
                 }
                 R.id.delete -> {
-                    Toast.makeText(context, "Delete Comment clicked", Toast.LENGTH_SHORT).show()
+                    deleteButtonClickListener?.onDeleteButtonClick(comment)
                     true
                 }
                 R.id.report -> {
-                    Toast.makeText(context, "Report Comment clicked", Toast.LENGTH_SHORT).show()
+                    reportButtonClickListener?.onReportButtonClick(comment)
                     true
                 }
                 else -> false
             }
         }
         popup.show()
+    }
+
+    private fun showToast(message: String) {
+        currentToast?.cancel()
+        currentToast = Toast.makeText(context, message, Toast.LENGTH_SHORT)
+        currentToast?.show()
     }
 }
